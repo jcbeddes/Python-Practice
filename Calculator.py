486c26688e48e7a1bafd369583fffba978b5f8eb
Jonah <jcbeddes@gmail.com>
2025-07-10 19:48:56 -0700

    Pull/push test

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,5 @@
+# Simple Calculator
+
 numone = int(input("Tell me a number: "))
 numtwo = int(input("Tell me another: "))
 operation = input("What operation do you want? (* / + -) ")
@@ -5,6 +7,10 @@ operation = input("What operation do you want? (* / + -) ")
 if operation == "*":
     print(numone * numtwo)
 elif operation == "/":
+    if numtwo == 0:
+        print("Error: Division by zero is not allowed.")
+        numtwo = int(input("Please enter a non-zero number: "))
+        print(numone / numtwo)
     print(numone / numtwo)
 elif operation == "+":
     print(numone + numtwo)
