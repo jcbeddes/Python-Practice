96665a9dfd6163ace6ed8a101ee726e7a7c58903
Jonah <jcbeddes@gmail.com>
2025-07-30 16:03:15 -0700

    more file testing

---
+++ filetesting.py
@@ -0,0 +1,12 @@
+with open('testing.txt', 'r') as f:
+
+    size_to_read = 10
+
+    f_contents = f.read(size_to_read)
+
+
+
+    # while len(f_contents) > 0:
+    #     print(f_contents, end="^")
+    #     f_contents = f.read(size_to_read)
+
